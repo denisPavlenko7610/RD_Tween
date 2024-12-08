ad21f182a2969e96e44f58a3e64a8e4101e5eac5
Denis <denispavlenko7610new@gmail.com>
2024-12-08 11:05:32 +0200

    ref

--- Runtime/TweenUpdater.cs
+++ Runtime/TweenUpdater.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RD_Tween.Runtime
 {
     public class TweenUpdater : MonoBehaviour
     {
-        private void Start()
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-
         void Update()
         {
-            TweenManager.Instance.Update(Time.deltaTime);
+            Tween.Instance.Update(Time.deltaTime);
         }
     }
 }
